688fbe38a22a456c0cfb0e5f387c5d85ffcb501b
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-11-12 16:13:55 +0300

    Add files via upload

---
+++ problem_1.js
@@ -0,0 +1,5 @@
+// write a function to print "Hello World From The Java Script" by using function method =>
+function displayingHelloMessage(){
+    return "Hello World From The Java Script.";
+};
+console.log(displayingHelloMessage());
