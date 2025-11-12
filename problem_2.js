5d3dfad5c448a9faf60c800869419f57428607d2
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-11-12 16:21:57 +0300

    Add files via upload

---
+++ problem_2.js
@@ -0,0 +1,7 @@
+// write a function that returns the sum of the two numbers by using java script language =>
+function sumOfTwoNumbers(fristNumber , secondNumber){
+    return "The Sum Of The Two Numbers Is = "+(fristNumber + secondNumber);
+}
+console.log(sumOfTwoNumbers(3 , 2));
+console.log(sumOfTwoNumbers(-3 , -6));
+console.log(sumOfTwoNumbers(7 , 3))
