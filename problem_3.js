866417f3b409facab48bd527fef7f7ca98b18581
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2026-03-06 21:27:11 +0300

    Add files via upload

---
+++ problem_3.js
@@ -0,0 +1,8 @@
+// write a function that takes an integer minutes , and then converted into seconds =>
+function convertedMinutesToSeconds(min){
+    console.log("The Number Of Minutes Is = ",min," Minutes");
+    return "The Number Of Seconds Is = "+min * 60+" Seconds";
+};
+console.log(convertedMinutesToSeconds(5)+"\n");
+console.log(convertedMinutesToSeconds(3)+"\n");
+console.log(convertedMinutesToSeconds(2)+"\n");
