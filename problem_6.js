95eda295283da88d70be223f5d4a349b4dcb3882
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2026-03-07 00:47:57 +0300

    Add files via upload

---
+++ problem_6.js
@@ -0,0 +1,8 @@
+// write a function to convert age from years into age for days =>
+function convertedAgeFromYearsToAgeToDays(years){
+    console.log("The Number Of Years Is = "+years+" Years")
+    return "The Number Of Days Is = "+years * 365+" Days";
+};
+console.log(convertedAgeFromYearsToAgeToDays(65)+"\n");
+console.log(convertedAgeFromYearsToAgeToDays(0)+"\n");
+console.log(convertedAgeFromYearsToAgeToDays(20));
