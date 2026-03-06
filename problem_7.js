e81c75206d12a186b7678b3cac5f8797543b09bf
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2026-03-07 00:54:10 +0300

    Add files via upload

---
+++ problem_7.js
@@ -0,0 +1,8 @@
+// write a function to get the cube of the integer number =>
+function cubeOfNumber(num){
+    console.log("The Number Is = ",num);
+    return "The Cube Of The Number Is = "+ num * num * num;
+};
+console.log(cubeOfNumber(3)+"\n");
+console.log(cubeOfNumber(5)+"\n");
+console.log(cubeOfNumber(10));
