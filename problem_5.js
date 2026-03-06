097ccdde77b9dc4317aafeb3123daa9fac89852f
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2026-03-06 21:52:36 +0300

    Add files via upload

---
+++ problem_5.js
@@ -0,0 +1,9 @@
+// write a function that takes the base , and height of treaingle , and then get the area of the traingle =>
+function areaOfTraingle(baseOfTra , heightOfTra){
+    console.log("The Base Of The Traingle Is = ",baseOfTra);
+    console.log("The Height Of The Traingle Is = ",heightOfTra);
+    return "The Area Of The Traingle Is = "+(1/2 * baseOfTra * heightOfTra);
+};
+console.log(areaOfTraingle(3 , 2)+"\n");
+console.log(areaOfTraingle(7 , 4)+"\n");
+console.log(areaOfTraingle(10 , 10))
